b68e0a65ae64d95e15f5290fd490ac63d19437c9
Toyin Jolaoso <oluwatoyinjolaoso@gmail.com>
2022-08-31 03:28:27 +0100

    change font-face file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,24 +6,24 @@ import Components from 'unplugin-vue-components/vite'
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   css: ['@/assets/style/css/main.css'],
-  buildModules: [
-    [
-      '@nuxtjs/google-fonts',
-      {
-        families: {
-          Quicksand: true,
-          'Qwitcher+Grypen': true,
-          subsets: ['latin'],
-          display: 'swap',
-          prefetch: false,
-          preconnect: false,
-          preload: false,
-          download: true,
-          base64: false,
-        },
-      },
-    ],
-  ],
+  // buildModules: [
+  //   [
+  //     '@nuxtjs/google-fonts',
+  //     {
+  //       families: {
+  //         Quicksand: true,
+  //         'Qwitcher+Grypen': true,
+  //         subsets: ['latin'],
+  //         display: 'swap',
+  //         prefetch: false,
+  //         preconnect: false,
+  //         preload: false,
+  //         download: true,
+  //         base64: false,
+  //       },
+  //     },
+  //   ],
+  // ],
   vite: {
     plugins: [
       Components({
